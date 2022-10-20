d4effb9a2377f3f5a5394c818ca578974ecbf8a5
OnyinyeNwafor <patrickskeemy@gmail.com>
2022-10-20 06:52:14 -0700

    fixed errors

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,26 +1,24 @@
+#include <stdio.h>
 #include "lists.h"
 
 /**
- * print_list - check the code for Holberton School students.
- * @h: name of the list
- * Return: the number of nodes.
+ * print_list - prints to standard output
+ * @h: A node in list_t
+ * Return: The number of nodes
  */
+
 size_t print_list(const list_t *h)
 {
-	int count = 0;
+	size_t num_of_nodes = 0;
 
 	while (h)
 	{
 		if (h->str == NULL)
-		{
 			printf("[0] (nil)\n");
-		}
 		else
-		{
-		printf("[%d] %s\n", h->len, h->str);
-		}
-		count++;
+			printf("[%u] %s\n", h->len, h->str);
 		h = h->next;
+		num_of_nodes++;
 	}
-	return (count);
+	return (num_of_nodes);
 }
